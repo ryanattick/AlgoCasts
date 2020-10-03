61449909f83b7edf45bd1c2b0e7801e36ad63bfc
Spencer Attick <spencer.attick@gmail.com>
2020-10-02 18:29:14 -0700

    reverseString solution

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,28 +6,50 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+
+
+
+
 function reverse(str) {
-  // return str.split('').reverse().join('');
-            // let strArr = str.split('');
-            // let counter = 1;
-            // while (counter < strArr.length) {
-            //   let temp = strArr[0];
-            //   strArr.shift();
-            //   strArr.splice(strArr.length - counter, 0, temp);
-            //   counter++;
-            // }
-            // let temp = strArr[strArr.length - 1];
-            // strArr.pop();
-            // strArr.unshift(temp);
-            // return strArr.join('');
-    // let reversed = '';
-    // for (let i = str.length - 1; i >= 0; i--) {
-    //   reversed += str[i];
-    // }
-    // return reversed;
-            return str.split('').reduce((acc, char) => {
-             return acc = char + acc;
-            }, '')
+  return str.split('').reverse().join('');
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function reverse(str) {
+//     return str.split('').reduce((acc, char) => {
+//      return acc = char + acc;
+//     }, '')
+// }
 
 module.exports = reverse;
